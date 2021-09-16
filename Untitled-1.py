3fa44313feac9944e77dd06c0f4bf662eca4c086
Carl521 <carl.westerlund@abo.fi>
2021-09-16 14:46:33 +0300

    created coffee class

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,10 @@
-print("hey")
+class Coffee:
+    def __init__(self, fullness, roastdegree, bitterness, aromastrength):
+        self.fullness = fullness
+        self.roastdegree = roastdegree
+        self.bitterness = bitterness
+        self.aromastrength = aromastrength
+
+x = Coffee(4, 3, 3, 2)
+
+
